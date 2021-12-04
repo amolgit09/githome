295530f630b39b00201fa6894eae2711da8dbdbe
amolgit09 <amol.wadekaredu@outlook.com>
2021-12-04 21:50:40 +0100

    Remove_error

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ module "vnet_setup" {
 
 module "keyVault_setup" {
    source = "./modules/keyvault_setup"
+   keyvault_name = "keyvault_terraform"
    resource_group_name = "terrform-demo-rg"
    location = "westeurope"
 }
