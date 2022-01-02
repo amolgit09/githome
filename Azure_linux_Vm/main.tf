3aec5c73e1e5a4473cd61922446aad2c1de09d37
amolgit09 <amol.wadekaredu@outlook.com>
2022-01-02 18:10:46 +0100

    modified:   Azure_linux_Vm/main.tf

--- Azure_linux_Vm/main.tf
+++ Azure_linux_Vm/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_network_interface" "main" {
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
-resource "azurerm_public_ip" "pip" "$${count.index}" {
+resource "azurerm_public_ip" "pip" {
   count               = local.instance_count
   name                = "${var.prefix}-pip${count.index}"
   resource_group_name = var.resource_group_name
