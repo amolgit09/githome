4f2faffcda023c2509f4aa444a1b78ba324c5bcd
amolgit09 <amol.wadekaredu@outlook.com>
2022-01-02 17:46:07 +0100

    modified:   Azure_linux_Vm/main.tf

--- Azure_linux_Vm/main.tf
+++ Azure_linux_Vm/main.tf
@@ -23,10 +23,10 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = var.subnet
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip"$${count.index}".id
   }
 }
-resource "azurerm_public_ip" "pip" {
+resource "azurerm_public_ip" "pip" "$${count.index}" {
   count               = local.instance_count
   name                = "${var.prefix}-pip${count.index}"
   resource_group_name = var.resource_group_name
