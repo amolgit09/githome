8a475142617f485e4ad6807d4bd6d8e452cb5a6c
amolgit09 <amol.wadekaredu@outlook.com>
2022-01-27 22:41:38 +0100

    Api devlopment

--- docker/test.py
+++ docker/test.py
@@ -1,4 +1,10 @@
-import os
+"""import os
 import socket
 host_name = socket.gethostname()
-print("I am from", host_name)
+print("I am from", host_name)"""
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
