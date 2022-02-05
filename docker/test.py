9514df671acd2edfb533fc17e9289d2be26b8c97
amolgit09 <amol.wadekaredu@outlook.com>
2022-02-06 00:44:03 +0100

    donewithjenkinsandgithub

--- docker/test.py
+++ docker/test.py
@@ -12,7 +12,7 @@ app = Flask(__name__)   # Flask constructor Need to update
 def hello():
     j = 1 + 1
     print ("",j)
-    print("This is Test for Jenkins with Github")
+    print("This is Test for Jenkins with Github is done")
     return 'HELLO This is Test'
   
 if __name__=='__main__':
